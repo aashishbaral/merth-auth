349dc079d66f4d6a1e18ff6ee777aac7e47edbdf
Aashish Baral <aashish.baral7@gmail.com>
2024-03-28 23:34:25 +0545

    signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 dotenv.config();
 
@@ -14,9 +15,12 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
 
 app.use("/api/user", userRoutes);
+
+app.use("/api/auth", authRoutes);
